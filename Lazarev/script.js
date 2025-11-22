3ac10b419324aec80fd09bcbcc262f658fb94b2e
Mack-0409 <mayanksingh0409@gmail.com>
2025-11-23 00:00:13 +0530

    improved nav bar

--- Lazarev/script.js
+++ Lazarev/script.js
@@ -1,1 +1,18 @@
+var nav = document.querySelector("nav")
 
+nav.addEventListener("mouseenter",function() {
+    var tl = gsap.timeline()
+
+    tl.to("#nav-bottom",{
+        height:"17vh"
+    })
+    tl.to(".nav-part2 h5",{
+        display:"block"
+    })
+     tl.from(".nav-part2 h5 span",{
+        y:12,
+        stagger:{
+            amount:0.6
+        }
+    })
+})
